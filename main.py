9514f6892815b6f5ec9cd9a7aa81de822f70144a
Gnusarov Vlad <gnusarovvladislav@gmail.com>
2023-01-27 11:36:41 +0300

    Fix wrong case

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ else:
 
 for i in range(1, 13):
     match i:
-        case 1, 3, 5, 7, 8, 10, 12:
+        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
             days = 31
-        case 4, 6, 9, 11:
+        case 4 | 6 | 9 | 11:
             days = 30
         case 2:
             days = visokosniy
